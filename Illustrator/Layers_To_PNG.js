9fb8d78fa49e100bb60f776ebcfff687859107fb
SR1 <sr1@SR1.local>
2019-03-14 15:48:53 -0500

    takes all layers no matter how many and exports to the size. User is asked for size to calc the percentage to export to

--- Illustrator/Layers_To_PNG.js
+++ Illustrator/Layers_To_PNG.js
@@ -1,76 +1,233 @@
-var destFolder, sourceFolder, files, fileType, sourceDoc, targetFile, epsSaveOpts, doc, layers;
+// Global Variables to use.
+var destFolder, sourceFolder, files, fileType, sourceDoc, targetFile, epsSaveOpts, doc, layers, vertScale, horzScale;
 
-// Select the source folder.
+// The folder where all of the Illustrator files are.
 sourceFolder = Folder.selectDialog( 'Select the folder with Illustrator files you want to convert to png', '~' );
+
+// Function that sets up the exporting process.
 function init(){
+
+		// Check to see if sourceFolder has any folders in it
 		if(sourceFolder != null){
+
+			// Create new array for files to be stored;
 			files = new Array();
+
+			// Only use file types that are illustrator files
 			fileType = "*.ai";
+
+			// Files get saved with all of the files in the sourcefolder
 			files = sourceFolder.getFiles( fileType );
 
+			// Check to see if there are any files in the folder
 			if(files.length > 0){
-				destFolder = Folder.selectDialog('Select the folder you want to export files to', '~');
-				for(var i = 0; i<files.length; i++){
-					sourceDoc = app.open(files[i]);
-					doc = app.activeDocument;
-					hideAllLayers();
-					exportLayers();
-					sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
 
+				// Ask for destination folder.
+				destFolder = Folder.selectDialog('Select the folder you want to export files to', '~');
+
+				// Loop through files
+				for(var i = 0; i<files.length; i++){
+					// Saves the open file and properties to sourceDoc
+					sourceDoc = app.open(files[i]);
+
+					doc = app.activeDocument;
+
+					// Get Bounds to the artboards
+					// Bounds work as such, left[0], top[1], right[2], bottom[3]
+					var bounds = doc.artboards[0].artboardRect;
+
+					var left = bounds[0];
+					var top = bounds[1];
+
+					// Subtract the right bounds from the left bounds to get width
+					var width = bounds[2] - left;
+
+					// Subtract the top bounds from the bottom bounds to get height;
+					var height = top - bounds[3];
+
+					// Prompts user for width and height values in pxs
+					askForDimensions(width, height);
+
+					// Hides all Layers
+					hideAllLayers();
+
+					// Exports all layers
+					exportLayers();
+					
+					// Use DONOTSAVECHANGES to keep PNG from adding other names to the file name
+					sourceDoc.close(SaveOptions.DONOTSAVECHANGES);
 				}
+			} else {
+				alert('There are no files here')
 			}
+		} else {
+			alert('There is no folder here')
 		}
 }
 
 
 function hideAllLayers(){
+	
+	// Loop throigh doc layers and turn them off
 	for(var l = 0; l < doc.layers.length; l++){
 		doc.layers[l].visible = false;
 	}
 }
 
+
+// Function asks user for width and height in pxs
+function askForDimensions(width, height){
+
+	// Create new dialog window
+	var w = new Window ("dialog", "Export PNG Scale");
+
+	// Group all inputs together
+	var scaleInputGroup = w.add("group");
+
+	// Put all inputs into a column
+	scaleInputGroup.orientation = "column";
+
+	// Group all width portions
+	var widthGroup = scaleInputGroup.add("group");
+
+	// Puts all width into a row
+	widthGroup.orientation = "row"
+
+	// Label for width
+	var widthLabel = widthGroup.add("statictext", undefined, "width in px");
+
+	// Editable text field for width
+	var widthField = widthGroup.add("edittext", undefined, "200");
+
+	// Sets number of characters allowed
+	widthField.characters = 20;
+
+	// Allows field to be editable
+	widthField.active = true;
+
+	// Group all height portions
+	var heightGroup = scaleInputGroup.add("group");
+
+	// Puts all height into a row
+	heightGroup.orientation = "row";
+
+	// Label for height
+	var heightLabel = heightGroup.add("statictext", undefined, "height in px");
+
+	// Editable text field for width
+	var heightField = heightGroup.add("edittext", undefined, "200");
+
+	// Sets number of characters allowed
+	heightField.characters = 20;
+
+	// Allows field to be editable
+	heightField.active = true;
+	
+
+	// Puts all buttons into a group
+	var buttonGroup = w.add("group");
+
+	// Sets all buttons into a row
+	buttonGroup.orientation = "row";
+
+	// Aligns buttons to the left side
+	buttonGroup.alignment = "left";
+
+	// Set confirm button
+	var confirmButton = buttonGroup.add("button", undefined, "OK");
+
+	// Create cancel button
+	buttonGroup.add("button", undefined, "Cancel")
+	
+	// Callback to after the button has been clicked
+	confirmButton.onClick = function(){
+
+		// Creates the scale for the height export
+		vscale = parseInt(heightField.text)/height;
+
+		// Creates the scale for the width export
+		hscale = parseInt(widthField.text)/width;
+
+		// Multiplies to increase outside of decimals
+		vertScale = vscale * 100;
+
+		// Multiples to increase outside of decimals
+		horzScale = hscale * 100;
+
+		// Closes the window
+		w.close()
+	}
+	// Shows the window
+	w.show ();
+}
+
+// Work horse for exporting layers
 function exportLayers(){
+
+	// Loop through layers
 	for(var e = 0; e< doc.layers.length; e++){
+
+		// Set selected layer on
 		doc.layers[e].visible = true;
+
+		// Store layerName
 		var layerName = doc.layers[e].name;
 
+		// Function returns a new file name
 		targetFile = getNewName(layerName);
+
+		// Returns PNG options
 		pngSaveOpts = getPNGOptions();
+
+		// Export file as PNG
 		sourceDoc.exportFile(targetFile,ExportType.PNG24,pngSaveOpts);
 
+		// Turn selected layer off
 		doc.layers[e].visible = false;
 
 	}
 }
 
-function getNewName(layerName)
-{
+function getNewName(layerName){
+	// Local Variables;
 	var ext, docName, newName, saveInFile, docName;
+	
+	// Set document name
 	docName = sourceDoc.name;
+	
+	// New document extention
 	ext = '.png'; 
+
+	// Sets new name;
 	newName = "";
+	
+	// Loop through document names
+	for ( var i = 0 ; docName[i] != "." ; i++ )	{
 		
-	for ( var i = 0 ; docName[i] != "." ; i++ )
-	{
+		// Set newName to document name;
 		newName += docName[i];
 	}
 	
+	// Set newName to documentName + underscore + layerName + extention;
 	newName  +=  '_' + layerName + ext;
 
+	// Save file to the dest folder with the new name;
 	saveInFile = new File( destFolder + '/' + newName );
 
 	return saveInFile;
 }
 
 
-function getPNGOptions()
-{
+// Function returns value of PNG settings
+function getPNGOptions(){
 	var pngSaveOpts = new ExportOptionsPNG24();
 	pngSaveOpts.antiAliasing = true;
 	pngSaveOpts.transparency = true;
 	pngSaveOpts.artBoardClipping = true;
-	pngSaveOpts.horizontalScale =  833.33;
-	pngSaveOpts.verticalScale = 833.33;
+
+	// horzScale and vertScale get set in the askForDimension Function
+	pngSaveOpts.horizontalScale =  horzScale;
+	pngSaveOpts.verticalScale = vertScale;
     	
 	return pngSaveOpts;	
 }
